e51efc139e615f2b2cca6eec31357569a5bcaff3
adebayor015 <adibpratama157@gmail.com>
2025-10-16 20:30:24 +0700

    database

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\BeritaController;
 use App\Models\Berita;  
+use App\Http\Controllers\MahasiswaController;
+
 
 use Illuminate\Support\Facades\Route;
 
@@ -19,23 +22,14 @@ Route::get('/profile', function () {
     ]);
 });
 
-Route::get('/berita', function () {
+Route::get('/berita', [BeritaController::class,'index']);
 
-    
-    return view('berita', [
-        "title" => "berita",
-        "beritas" => Berita ::ambildata(),
-    ]);
-});
 
-Route::get('/berita/{slug}', function ($slugp) {
-    
-   
-    return view('singleberita', [
-        "title" => "berita",
-        "new_berita" => Berita ::caridata($slugp),
-    ]);
-});
+Route::get('/berita/{slug}', [BeritaController::class, 'phptampildata']); 
+
+Route::get('/mahasiswa', [MahasiswaController::class,'index']);
+
+
 Route::get('/contact', function () {
     return view('contact', [
         "title" => "contact",
